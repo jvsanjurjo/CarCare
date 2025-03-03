d16178dc80ab7e983430a57268fc3d99a060d7d7
vsanjurjo <vsanjurjo@outlook.com>
2025-03-03 13:35:14 -0300

    dependencyinjection added to api project

---
+++ CareCare.Api/Controllers/CarController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CareCare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        // GET: api/<CarController>
+        [HttpGet("listAll")]
+        public IEnumerable<string> ListCars()
+        {
+            return new string[] { "car1", "car2" };
+        }
+
+        // GET api/<CarController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CarController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CarController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CarController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CareCare.Api/DependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿namespace CareCare.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
