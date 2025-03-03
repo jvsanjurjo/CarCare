319c44f30432eed840913bd41a0cb6e896dc1c6c
vsanjurjo <vsanjurjo@outlook.com>
2025-03-03 19:34:21 -0300

    car command and query added

---
+++ CarCare.Application/Cars/Commands/CarCommand.cs
@@ -0,0 +1,34 @@
+﻿using CarCare.Application.Common.Interfaces.Persistence;
+using CarCare.Domain.Entities;
+using MediatR;
+
+namespace CarCare.Application.Cars.Commands
+{
+    public class CreateCarCommand : IRequest
+    {
+        public string RegistrationNumber { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public string ModelYear { get; set; } = null!;
+    }
+
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand>
+    {
+        private readonly ICarRepository _carRepository;
+        public CreateCarCommandHandler(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public async Task Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            var car = new Car
+            {
+                RegistrationNumber = request.RegistrationNumber,
+                Model = request.Model,
+                ModelYear = request.ModelYear
+            };
+
+            await Task.Run(() => _carRepository.AddCar(car));
+
+        }
+    }
+}
---
+++ CarCare.Application/Cars/Queries/CarQuery.cs
@@ -0,0 +1,38 @@
+﻿using CarCare.Application.Common.Interfaces.Persistence;
+using MediatR;
+
+namespace CarCare.Application.Cars.Queries
+{
+    public class GetCarQuery:IRequest<GetCarQueryResponse>
+    {
+        public string RegistrationNumber { get; set; }
+    }
+
+    public class GetCarQueryHandler: IRequestHandler<GetCarQuery, GetCarQueryResponse>
+    {
+        private readonly ICarRepository _carRepository;
+        public GetCarQueryHandler(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public async Task<GetCarQueryResponse> Handle(GetCarQuery request, CancellationToken cancellationToken)
+        {
+            var car = _carRepository.GetCarByRegistrationNumber(request.RegistrationNumber);
+            return new GetCarQueryResponse
+            {
+                Id = car.Id,
+                RegistrationNumber = car.RegistrationNumber,
+                Model = car.Model,
+                ModelYear = car.ModelYear
+            };
+        }
+    }
+
+    public class GetCarQueryResponse
+    {
+        public Guid Id { get; set; }
+        public string RegistrationNumber { get; set; } = default!;
+        public string Model { get; set; } = default!;
+        public string ModelYear { get; set; } = default!;
+    }
+}
