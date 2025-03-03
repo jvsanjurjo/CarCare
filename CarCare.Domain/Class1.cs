64621caa2b3496caa7df77b3b9ad4681756fce78
vsanjurjo <vsanjurjo@outlook.com>
2025-03-03 13:07:39 -0300

    car entity created

--- CarCare.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CarCare.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CarCare.Domain/Entities/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace CarCare.Domain.Entities
+{
+    public class Car
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Model { get; set; } = null!;
+        public string ModelYear { get; set; } = null!;
+    }
+}
